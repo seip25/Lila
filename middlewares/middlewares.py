9d691d9e4b04912da6d88d49bccbb7f547cdaad1
seip25 <andrespaivasilveiraa@gmail.com>
2025-01-18 00:02:57 -0300

    example middleware

--- middlewares/middlewares.py
+++ middlewares/middlewares.py
@@ -2,10 +2,10 @@ from core.session import Session
 from core.responses import RedirectResponse
 from functools import wraps
 
-def login_required(func):
+def login_required(func,key:str='auth'):
     @wraps(func)
     async def wrapper(request, *args, **kwargs):
-        session_data= Session.unsign(key="auth",request=request)
+        session_data= Session.unsign(key=key,request=request)
         if not session_data:
             return RedirectResponse(url='/login')
         return await func(request,*args,**kwargs)
@@ -14,6 +14,6 @@ def login_required(func):
 def check_token(func):
     @wraps(func)
     async def wrapper(request, *args, **kwargs):
-        #Validate token and id for headers in db
+        #Validate token and id for headers in db or use jwt 
         return await func(request,*args,**kwargs)
     return wrapper
