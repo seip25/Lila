0d2a2809afcb13273f43655376b89d94853e000c
seip25 <andrespaivasilveiraa@gmail.com>
2025-01-18 00:00:11 -0300

    example input fetch in aapi

--- routes/api.py
+++ routes/api.py
@@ -12,21 +12,40 @@ async def api(request : Request):
      return JSONResponse({'api':True})
 
 #Example Pydantic
-class InputModel(BaseModel):
+class LoginModel(BaseModel):
     email : EmailStr
     password: str
 
-@router.route(path='/input',methods=['POST'])
+@router.route(path='/login',methods=['POST'])
 async def login(request:Request):
     msg= translate(file_name='guest',request=request)
     msg_error=msg['Incorrect email or password']
     body = await request.json()
-    input=InputModel(**body)
+    input=LoginModel(**body)
     email = input.email
     password = input.password 
-    print(input)
     response=JSONResponse({"success":False,"email":email,"password":password,"msg":msg_error})
     return response
-   
+
+
+
+#Example Pydantic
+class RegisterModel(BaseModel):
+    email : EmailStr
+    password: str
+    name :str
+    password_2 :str
+
+@router.route(path='/register',methods=['POST'])
+async def login(request:Request):
+    body = await request.json()
+    input=RegisterModel(**body)
+    name=input.name
+    email = input.email
+    password = input.password 
+    password_2=input.password_2
+    response=JSONResponse({"success":False,"email":email,"password":password,"name":name,"password_2":password_2})
+    return response
+      
 
 routes=router.get_routes()
--- static/js/login.js
+++ static/js/login.js
@@ -1,7 +1,7 @@
 async function Login(event) {
     event.preventDefault();
     const form = Object.fromEntries(new FormData(event.target))
-    const resp=await Http('/input', 'POST', form)
+    const resp=await Http('/login', 'POST', form)
     console.log(resp)
 
     alert(JSON.stringify(resp))
