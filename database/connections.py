ef1e1e3040f3a44de61ed62216dab9a14d531cfc
Andres Paiva <87265390+seip25@users.noreply.github.com>
2025-01-19 18:34:30 -0300

    Update connections.py

--- database/connections.py
+++ database/connections.py
@@ -1,11 +1,11 @@
 from core.database import Database
 
 
-# config = {"type":"sqlite","database":"test"} #test.db
-# connection = Database(config=config)
-# connection.connect()
-
-config = {"type":"mysql","host":"127.0.0.1","user":"root","password":"password","database":"db_test","auto_commit":True}
+config = {"type":"sqlite","database":"test"} #test.db
 connection = Database(config=config)
 connection.connect()
-mysql_connection = connection
+
+#config = {"type":"mysql","host":"127.0.0.1","user":"root","password":"password","database":"db_test","auto_commit":True}
+#connection = Database(config=config)
+#connection.connect()
+#mysql_connection = connection
