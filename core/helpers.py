f59cffe26c7b48f118522db20b4bd572a69afec5
Andres Paiva <87265390+seip25@users.noreply.github.com>
2025-01-22 16:08:38 -0300

    Update helpers.py

--- core/helpers.py
+++ core/helpers.py
@@ -47,10 +47,17 @@ def generate_token(name:str,value:str)->str :
     return token
 
 def get_token(token:str):
-    token = token.strip().split(" ")[1] 
     try:
-        return jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        token=token.strip().split(" ")
+        if len(token)>1:
+            token = token[1] 
+            return jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        return JSONResponse({'session':False,'message': 'Invalid token'},status_code=401)
     except jwt.ExpiredSignatureError:
+        return JSONResponse({'session':False,'message': 'Token has expired'}, status_code=401) 
+    except jwt.InvalidTokenError:
+        return JSONResponse({'session':False,'message': 'Invalid token'},status_code=401)
+
         return JSONResponse({'session':False,'message': 'Token has expired'}, status_code=401) 
     except jwt.InvalidTokenError:
         return JSONResponse({'session':False,'message': 'Invalid token'},status_code=401)
