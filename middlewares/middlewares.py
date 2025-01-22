f7cf8c09049cda927fe1bfb0847fb38a353482e9
Andres Paiva <87265390+seip25@users.noreply.github.com>
2025-01-22 15:46:18 -0300

    Update middlewares.py

--- middlewares/middlewares.py
+++ middlewares/middlewares.py
@@ -3,7 +3,7 @@ from core.responses import RedirectResponse,JSONResponse
 from core.env import SECRET_KEY
 from core.request import Request
 from functools import wraps 
-import jwt
+from core.helpers import get_token
 
 def login_required(func,key:str='auth'):
     @wraps(func)
@@ -28,14 +28,9 @@ def validate_token(func):
     async def wrapper(request: Request, *args, **kwargs):
         token = request.headers.get('Authorization')
         if not token:
-            return JSONResponse({'message': 'Invalid token'},status_code=401)
-        try:
-            token = token.split(" ")[1] 
-            decoded_token = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-            return await func(request, *args, **kwargs)
-        except jwt.ExpiredSignatureError:
-            return JSONResponse({'message': 'Token has expired'}, status_code=401) 
-        except jwt.InvalidTokenError:
-            return JSONResponse({'message': 'Invalid token'},status_code=401)
-
+            return JSONResponse({'session':False,'message': 'Invalid token'},status_code=401)
+        
+        token = get_token(token=token)
+        return await func(request, *args, **kwargs)
+       
     return wrapper
