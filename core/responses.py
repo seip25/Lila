246ea0467dc3e0c49e3e09fe794192536eb0152c
Andres Paiva <87265390+seip25@users.noreply.github.com>
2025-02-28 16:28:08 -0300

    Update responses.py

--- core/responses.py
+++ core/responses.py
@@ -1,8 +1,29 @@
-from starlette.responses import JSONResponse,HTMLResponse,RedirectResponse,PlainTextResponse,RedirectResponse,StreamingResponse
+from starlette.responses import  JSONResponse as StarletteJSONResponse,HTMLResponse,RedirectResponse,PlainTextResponse,RedirectResponse,StreamingResponse
+from decimal import Decimal
+from datetime import date, datetime
 
-JSONResponse = JSONResponse
+ 
 HTMLResponse = HTMLResponse
 RedirectResponse = RedirectResponse
 PlainTextResponse = PlainTextResponse
 RedirectResponse=RedirectResponse
 StreamingResponse=StreamingResponse
+
+def convert_to_serializable(obj):
+    if isinstance(obj, (Decimal)):
+        return float(obj) 
+    elif isinstance(obj, (date, datetime)):
+        return obj.isoformat()  
+    elif isinstance(obj, dict):
+        return {key: convert_to_serializable(value) for key, value in obj.items()}
+    elif isinstance(obj, (list, tuple)):
+        return [convert_to_serializable(item) for item in obj]
+    elif hasattr(obj, "__dict__"):  
+        return convert_to_serializable(obj.__dict__)
+    else:
+        return obj 
+
+class JSONResponse(StarletteJSONResponse):
+    def __init__(self, data, status_code=200):
+        serialized_data = convert_to_serializable(data)
+        super().__init__(serialized_data, status_code=status_code)
