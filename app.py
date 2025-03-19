fb7105e8acd5167e323235cca29139ad3638b258
Andres Paiva <andrespaivasilveiraa@gmail.com>
2025-03-18 22:17:58 -0300

    admin panel and docs

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ all_routes = list(itertools.chain(routes, api_routes))
 
 # English: Here we activate the admin panel with default settings.
 # Español: Aquí activamos el panel de administrador con configuraciones predeterminadas.
-#from core.admin import Admin
+# from core.admin import Admin
 # from models.user import User
-# admin_routes=Admin(models=[User],prefix="admin",user_default="admin")
+# admin_routes=Admin(models=[User],user_default="administrador")
 # all_routes = list(itertools.chain(routes, api_routes,admin_routes))
 
 # English: Initializing the application with debugging enabled and the combined routes.
