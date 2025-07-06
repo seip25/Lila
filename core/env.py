e78445a070d81dc964b44f62743247d3e0b1351a
Andres Paiva <andrespaivasilveiraa@gmail.com>
2025-07-05 21:30:27 -0300

    Update env.py

--- core/env.py
+++ core/env.py
@@ -1,10 +1,9 @@
 from dotenv import load_dotenv
-from os import getenv,path
+from os import getenv, path
+import os
 
-BASE_DIR = path.abspath(path.dirname(__file__))
-BASE_DIR = path.dirname(BASE_DIR) 
-env =path.join(BASE_DIR, '.env')
-load_dotenv(dotenv_path=env,encoding='utf-8')
+env_path = path.join(os.getcwd(), '.env')
+load_dotenv(dotenv_path=env_path, encoding='utf-8')
 
 TITLE_PROJECT = getenv('TITLE_PROJECT', 'title project')
 VERSION_PROJECT = getenv('VERSION_PROJECT', 1)  
@@ -13,5 +12,5 @@ THEME_DEFAULT = getenv('THEME_DEFAULT', 'blue')
 LANG_DEFAULT = getenv('LANG_DEFAULT', 'en')
 SECRET_KEY = getenv('SECRET_KEY', '')
 PORT = int(getenv('PORT', 8001))  
-HOST = getenv('HOST', '127.0.0.1') 
-DEBUG = getenv('DEBUG', 'False').lower() in ('true', '1', 'yes')
+HOST = getenv('HOST', '127.0.0.1')
+DEBUG = getenv('DEBUG', 'True').lower() in ('true', '1', 'yes')
