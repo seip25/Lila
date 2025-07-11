5730c699942c9609d429a01bea1b772353c86fbf
Andres Paiva <andrespaivasilveiraa@gmail.com>
2025-07-11 14:54:54 -0300

    anime.js

---
+++ docs/js/anime.js
@@ -0,0 +1,48 @@
+window.addEventListener('DOMContentLoaded',()=>{
+    Animations.easeOutBack('div,p,h1,h3,h4,h5,h2,article, i', 300);
+})
+
+const Animations = {
+    fadeUp: (selector, delay = 0) => {
+        return anime({
+            targets: selector,
+            translateY: [40, 0],
+            opacity: [0, 1],
+            duration: 800,
+            delay: delay,
+            easing: 'easeOutQuint'
+        });
+    },
+
+    fadeIn: (selector, delay = 0) => {
+        return anime({
+            targets: selector,
+            opacity: [0, 1],
+            duration: 600,
+            delay: delay,
+            easing: 'easeInOutQuad'
+        });
+    },
+
+    staggerFadeUp: (selector) => {
+        return anime({
+            targets: selector,
+            translateY: [30, 0],
+            opacity: [0, 1],
+            delay: anime.stagger(200, { start: 200 }),
+            duration: 700,
+            easing: 'easeOutBack'
+        });
+    },
+    easeOutBack: (selector, delay = 300) => {
+        anime({
+            targets: selector,
+            translateY: [15, 0],
+            opacity: [0, 1],
+            scale: [0.9, 1],
+            delay: anime.stagger(80, { start: delay }),
+            duration: 600,
+            easing: 'easeOutBack'
+        });
+    }
+};
